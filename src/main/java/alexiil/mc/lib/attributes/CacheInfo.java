55a42d5e431744b5caf84a2f1e30f867300f950f
AlexIIL <alexj9@me.com>
2019-03-02 23:14:15 +0000

    Fix a crash caused by CacheInfo.NOT_CACHABLE always being null.

--- src/main/java/alexiil/mc/lib/attributes/CacheInfo.java
+++ src/main/java/alexiil/mc/lib/attributes/CacheInfo.java
@@ -2,7 +2,7 @@ package alexiil.mc.lib.attributes;
 
 public final class CacheInfo {
 
-    public static final CacheInfo NOT_CACHABLE = null;
+    public static final CacheInfo NOT_CACHABLE = new CacheInfo();
 
     // TODO: Information and listeners for this cache!
 }
