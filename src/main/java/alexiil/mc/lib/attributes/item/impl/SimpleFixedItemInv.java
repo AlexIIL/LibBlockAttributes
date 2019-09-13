3b99f8bc3513b68c7cd6ba22e33fca3059245611
AlexIIL <alexj9@me.com>
2019-09-13 19:09:11 +0100

    Add a deprecated "SimpleFixedItemInv" which as a big deprecation warning on both the class and it's singular constructor.
    (Plus the constructor throws an exception).

---
+++ src/main/java/alexiil/mc/lib/attributes/item/impl/SimpleFixedItemInv.java
@@ -0,0 +1,56 @@
+package alexiil.mc.lib.attributes.item.impl;
+
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+
+import alexiil.mc.lib.attributes.item.FixedItemInv.CopyingFixedItemInv;
+import alexiil.mc.lib.attributes.item.FixedItemInv.ModifiableFixedItemInv;
+import alexiil.mc.lib.attributes.item.FixedItemInvView;
+
+/** @deprecated You should use either {@link DirectFixedItemInv} or {@link FullFixedItemInv} instead of this!
+ *             <p>
+ *             Full explanation: As of LBA 0.5.0 there's been a large change to how inventories work to be much closer
+ *             to vanilla - inventories no longer return immutable stacks from
+ *             {@link FixedItemInvView#getInvStack(int)}, and 2 new sub-interfaces have been created to handle the
+ *             different ways of using this, either {@link ModifiableFixedItemInv} (which exposes a markDirty() method
+ *             just like vanilla {@link Inventory} does), or {@link CopyingFixedItemInv} (which returns copies of the
+ *             internal {@link ItemStack} and supports complete filtering and listener capabilities).
+ *             <p>
+ *             You should replace this class with either:
+ *             <ul>
+ *             <li>{@link FullFixedItemInv} if you either need exact per-slot listening or the full filtration
+ *             capabilities</li>
+ *             <li>{@link DirectFixedItemInv} if you'd rather use a more vanilla oriented approach of being able to
+ *             directly modify items in an inventory.</li>
+ *             </ul>
+ */
+@Deprecated
+public class SimpleFixedItemInv {
+
+    // Copy-pasted from the top
+    /** @deprecated You should use either {@link DirectFixedItemInv} or {@link FullFixedItemInv} instead of this!
+     *             <p>
+     *             Full explanation: As of LBA 0.5.0 there's been a large change to how inventories work to be much
+     *             closer to vanilla - inventories no longer return immutable stacks from
+     *             {@link FixedItemInvView#getInvStack(int)}, and 2 new sub-interfaces have been created to handle the
+     *             different ways of using this, either {@link ModifiableFixedItemInv} (which exposes a markDirty()
+     *             method just like vanilla {@link Inventory} does), or {@link CopyingFixedItemInv} (which returns
+     *             copies of the internal {@link ItemStack} and supports complete filtering and listener capabilities).
+     *             <p>
+     *             You should replace this class with either:
+     *             <ul>
+     *             <li>{@link FullFixedItemInv} if you either need exact per-slot listening or the full filtration
+     *             capabilities</li>
+     *             <li>{@link DirectFixedItemInv} if you'd rather use a more vanilla oriented approach of being able to
+     *             directly modify items in an inventory.</li>
+     *             </ul>
+     */
+    @Deprecated
+    public SimpleFixedItemInv(int invSize) {
+        throw new IncompatibleClassChangeError(
+            "FixedItemInv changed a lot in LBA 0.5.x\n"//
+                + "\t\tThe mod author should refer to either the changelog or\n"
+                + "\t\tthe deprecated explanation in SimpleFixedItemInv for what to change!"
+        );
+    }
+}
