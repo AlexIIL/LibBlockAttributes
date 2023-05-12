036cb93274cae41627da357360d4151ce336e4e8
Kneelawk <kneelawk@gmail.com>
2023-05-12 11:03:50 -0700

    Remove redundant calls

--- src/main/java/alexiil/mc/lib/attributes/item/compat/FixedInventoryVanillaWrapper.java
+++ src/main/java/alexiil/mc/lib/attributes/item/compat/FixedInventoryVanillaWrapper.java
@@ -29,7 +29,8 @@ public class FixedInventoryVanillaWrapper extends FixedInventoryViewVanillaWrapp
     public boolean setInvStack(int slot, ItemStack to, Simulation simulation) {
         boolean allowed = false;
         ItemStack current = getInvStack(slot);
-        if (to.isEmpty()) {
+        boolean removing = to.isEmpty();
+        if (removing) {
             allowed = canExtract(slot, current);
         } else {
             if (current.isEmpty()) {
@@ -46,7 +47,7 @@ public class FixedInventoryVanillaWrapper extends FixedInventoryViewVanillaWrapp
         }
         if (allowed) {
             if (simulation == Simulation.ACTION) {
-                if (to.isEmpty()) {
+                if (removing) {
                     inv.removeStack(slot);
                 } else {
                     inv.setStack(slot, to);
